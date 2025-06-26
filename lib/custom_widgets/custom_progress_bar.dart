0b6cab395969c8a5e065e26b82c1f1e3af39dcdf
jenilkhatrani <149984350+jenilkhatrani@users.noreply.github.com>
2025-06-26 12:51:55 +0530

    Progress Bar

---
+++ lib/custom_widgets/custom_progress_bar.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+import '../themes_colors/colors.dart';
+import 'custom_inner_shadow.dart';
+
+class CustomSegmentedProgressBar extends StatelessWidget {
+  final int totalSegments;
+  final int percentComplete;
+  final List<Color> gradientColors;
+  final double? height;
+  final double? width;
+  final double spacing;
+  final Color? defaultColor;
+
+  const CustomSegmentedProgressBar({
+    super.key,
+    required this.totalSegments,
+    required this.percentComplete,
+    required this.gradientColors,
+    this.height = 12,
+    this.spacing = 2,
+    this.width = double.infinity,
+    this.defaultColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (percentComplete > 100) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Progress cannot exceed 100%"),
+            backgroundColor: Colors.red,
+          ),
+        );
+      });
+    }
+
+    final progress = (percentComplete.clamp(0, 100)) / 100.0;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final totalWidth = constraints.maxWidth;
+        final segmentWidth =
+            (totalWidth - spacing * (totalSegments - 1)) / totalSegments;
+
+        return Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(totalSegments, (index) {
+                return Container(
+                  width: segmentWidth,
+                  height: height,
+                  decoration: BoxDecoration(
+                    color: defaultColor ?? Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(height ?? 12 / 2),
+                  ),
+                );
+              }),
+            ),
+            ClipRect(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                widthFactor: progress.clamp(0.0, 1.0),
+                child: ShaderMask(
+                  shaderCallback: (bounds) {
+                    return LinearGradient(colors: gradientColors).createShader(
+                      Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(totalSegments, (index) {
+                      return InnerShadowContainer(
+                        height: height,
+                        width: segmentWidth,
+                        borderRadius: (height ?? 12 / 2),
+                        backgroundColor: Colors.white,
+                        isShadowBottomRight: true,
+                        alignment: Alignment.center,
+                        child: const SizedBox(),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+            ),
+
+            // ClipRect(
+            //   child: Align(
+            //     alignment: Alignment.centerLeft,
+            //     widthFactor: progress.clamp(0.0, 1.0),
+            //     child: ShaderMask(
+            //       shaderCallback: (bounds) {
+            //         return LinearGradient(colors: gradientColors).createShader(
+            //           Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+            //         );
+            //       },
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //         children: List.generate(totalSegments, (index) {
+            //           return Container(
+            //             width: segmentWidth,
+            //             height: height,
+            //             decoration: BoxDecoration(
+            //               color: Colors.white,
+            //               borderRadius: BorderRadius.circular(height ?? 12 / 2),
+            //             ),
+            //           );
+            //         }),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Positioned(
+              left: (progress * totalWidth) - 20,
+              top: -50,
+              child: Column(
+                children: [
+                  InnerShadowContainer(
+                    height: 40,
+                    width: 40,
+                    backgroundColor: AppColors.primary,
+                    borderRadius: 50,
+                    isShadowBottomLeft: true,
+                    child: Text(
+                      "$percentComplete%",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // isShadowTopRight: true,
+                  ),
+                  CustomPaint(
+                    size: const Size(10, 8),
+                    painter: _BubblePointerPainter(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _BubblePointerPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = const Color(0xFF0F4C75);
+    final path = Path()
+      ..moveTo(0, -2)
+      ..lineTo(size.width / 2, size.height)
+      ..lineTo(size.width, -2)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
